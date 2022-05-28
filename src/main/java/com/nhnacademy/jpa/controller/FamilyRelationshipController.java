8b3eb9aa3c7cb4187033850cd7f88de2925263a1
윤동열 <eastheat10@gmail.com>
2022-05-29 01:21:29 +0900

    feat: 가족관계 수정

--- src/main/java/com/nhnacademy/jpa/controller/FamilyRelationshipController.java
+++ src/main/java/com/nhnacademy/jpa/controller/FamilyRelationshipController.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.jpa.controller;
 
 import com.nhnacademy.jpa.dto.request.family_relationship.FamilyRelationshipInsertRequest;
+import com.nhnacademy.jpa.dto.request.family_relationship.FamilyRelationshipModifyRequest;
 import com.nhnacademy.jpa.dto.request.family_relationship.FamilyRelationshipRequest;
 import com.nhnacademy.jpa.dto.response.FamilyRelationshipResponse;
 import com.nhnacademy.jpa.service.FamilyRelationshipService;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,6 +43,30 @@ public class FamilyRelationshipController {
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         return ResponseEntity.status(HttpStatus.CREATED)
+                             .headers(headers)
+                             .body(familyRelationshipResponse);
+    }
+
+    @PutMapping("/{serialNumber}/relationship/{familySerialNumber}")
+    public ResponseEntity<FamilyRelationshipResponse> modify(
+        @PathVariable("serialNumber") Long serialNumber,
+        @PathVariable("familySerialNumber") Long familySerialNumber,
+        @RequestBody FamilyRelationshipModifyRequest modifyRequest) {
+
+        FamilyRelationshipRequest relationshipRequest =
+            FamilyRelationshipRequest.builder()
+                                     .serialNumber(serialNumber)
+                                     .familySerialNumber(familySerialNumber)
+                                     .relationship(modifyRequest.getRelationship())
+                                     .build();
+
+        FamilyRelationshipResponse familyRelationshipResponse =
+            familyRelationshipService.updateFamilyRelationship(relationshipRequest);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        return ResponseEntity.status(HttpStatus.OK)
                              .headers(headers)
                              .body(familyRelationshipResponse);
     }
